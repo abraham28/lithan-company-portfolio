b49976457aa6009b6e93ba84f789e88b826dcace
abraham28 <abrahampoog@gmail.com>
2020-01-03 22:24:20 +0800

    initial commit

---
+++ about_us/index.php
@@ -0,0 +1,21 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/header.html"); ?>
+
+
+<section class="section01">
+    <div class="sec01Blk01">
+        <h1 class="aboutUsTitle">
+            About<br>
+            ABC Learning Center
+        </h1>
+        <div class="aboutUsBlk01">
+            ABC Learning Center is an IT Training
+            institute that conducts courses like Programming in Java, Programming in .Net,
+            Microsoft Office, HTML etc.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic eaque, fugit placeat, et magni sit! Iure vel cumque ipsam. Rem atque, accusantium fugiat ratione. Consectetur harum nostrum sequi, voluptas atque?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad tempora et, officiis, libero vitae consequatur quas quidem dolores at dolorum ipsa veniam maiores quasi. Enim ut temporibus sequi id illum.
+        </div>
+    </div>
+</section>
+
+
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/footer.html"); ?>
---
+++ sitemap/index.php
@@ -0,0 +1,15 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/header.html"); ?>
+
+
+<section class="section01">
+    <div class="sec01Blk01">
+        <h1 class="sec01Blk01Area01">
+            <span class="left">ABC<br></span>
+            <span class="center">Learning<br></span>
+            <span class="end">Center</span>
+        </h1>
+    </div>
+</section>
+
+
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/footer.html"); ?>
